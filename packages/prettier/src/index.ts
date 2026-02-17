376fb3f375df616420dd0c71c5385202c9ac1490
Sviatoslav <sviatoslavvww@gmail.com>
2026-02-17 23:28:39 +0200

    first commit

---
+++ packages/prettier/src/index.ts
@@ -0,0 +1,13 @@
+import type { Config as Prettier } from 'prettier';
+
+export const prettier: Prettier = {
+  printWidth: 100,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  arrowParens: 'always',
+};
